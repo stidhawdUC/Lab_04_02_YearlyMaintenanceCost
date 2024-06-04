b260c19743b5f76bc18bc745924e6d32498d9b14
worleystidham <stidhawd@mail.uc.edu>
2024-06-03 21:05:58 -0400

    take 1

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        // let's do this
+        double costWinter = 0.00;
+        double costSpring = 0.00;
+        double costSummer = 0.00;
+        double costFall = 0.00;
+        double costYear = 0.00;
+
+        System.out.println("Please enter your home maintenance costs for Winter.");
+        costWinter = 3500.43;
+
+        System.out.println("You entered " + costWinter + " for Winter.  Please enter home " +
+                "maintenance costs for Spring.");
+        costSpring = 472.72;
+
+        System.out.println("You entered " + costSpring + " for Spring.  Please enter home " +
+                "maintenance costs for Summer.");
+        costSummer = 1141.29;
+
+        System.out.println("You entered " + costSummer + " for Summer.  Please enter home " +
+                "maintenance costs for Fall.");
+        costFall = 4843.91;
+
+        costYear = costWinter + costSpring + costSummer + costFall;
+
+        System.out.println("Your seasonal totals are " + costWinter + " for " +
+                "Winter, " + costSpring + " for Spring, " + costSummer + " for Summer, and " + costFall + " for " +
+                "Fall.");
+        System.out.println("Your yearly maintenance costs total " + costYear + ".");
+
+    }
+}
